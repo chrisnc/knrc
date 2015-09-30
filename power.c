a51d64f6fa7833bbc6eda503481e8bbdfea2011a
Chris Copeland <chrisnc@cs.stanford.edu>
2015-09-29 20:28:05 -0700

    add 2nd version of power

--- power.c
+++ power.c
@@ -13,10 +13,23 @@ int main(void)
 }
 
 // power: raise base to n-th power; n >= 0
+/*
 int power(int base, int n)
 {
   int p = 1;
   for (int i = 1; i <= n; ++i)
+  {
+    p = p * base;
+  }
+  return p;
+}
+*/
+
+// power: raise base to n-th power; n >= 0; version 2
+int power(int base, int n)
+{
+  int p;
+  for (p = 1; n > 0; --n)
   {
     p = p * base;
   }
