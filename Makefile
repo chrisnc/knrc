f33c2cc62736698e0e73663fbd7bb718c71c8b75
Chris Copeland <chrisnc@cs.stanford.edu>
2015-09-29 20:24:45 -0700

    add power example

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ TARGETS = \
 	temperature \
 	chario \
 	wc \
-	array
+	array \
+	power
 
 all: $(TARGETS)
 
---
+++ power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int base, int n);
+
+// test power function
+int main(void)
+{
+  for (int i = 0; i < 10; ++i)
+  {
+    printf("%d %d %d\n", i, power(2,i), power(-3,i));
+  }
+  return 0;
+}
+
+// power: raise base to n-th power; n >= 0
+int power(int base, int n)
+{
+  int p = 1;
+  for (int i = 1; i <= n; ++i)
+  {
+    p = p * base;
+  }
+  return p;
+}
