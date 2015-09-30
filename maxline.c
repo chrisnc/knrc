11ed449cee8cee132c0135d81c16ee182431503e
Chris Copeland <chrisnc@cs.stanford.edu>
2015-09-29 21:22:39 -0700

    add exercise 1-18

--- maxline.c
+++ maxline.c
@@ -7,38 +7,51 @@ int my_getline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 // print the longest input line
-int main(void)
+void get_max_line(void)
 {
-  int len; // current line length
-  int max; // maximum length seen so far
-  char line[MAXLINE]; // current input line
+  int len;               // current line length
+  int max;               // maximum length seen so far
+  char line[MAXLINE];    // current input line
   char longest[MAXLINE]; // longest line saved here
 
   max = 0;
   while ((len = my_getline(line, MAXLINE)) > 0)
   {
-    if (len > max) {
+    if (len > max)
+    {
       max = len;
       copy(longest, line);
     }
   }
   if (max > 0) // there was a line
   {
+    printf("the longest line was %d characters\n", max);
     printf("%s", longest);
   }
-  return 0;
 }
 
 // getline: read a line into s, return length
+// Exercise 1-16. modified to return the actual length of the line, even if it
+// was longer than lim-1
 int my_getline(char s[], int lim)
 {
   int c = 0, i;
 
-  for (i = 0; i < lim - 1 && c != '\n' && (c = getchar()) != EOF; ++i)
+  for (i = 0; c != '\n' && (c = getchar()) != EOF; ++i)
   {
-    s[i] = c;
+    if (i < lim - 1)
+    {
+      s[i] = c;
+    }
   }
-  s[i] = '\0';
+  if (i < lim)
+  {
+    s[i] = '\0';
+  }
+  else
+  {
+    s[lim - 1] = '\0';
+  }
   return i;
 }
 
@@ -50,4 +63,53 @@ void copy(char to[], char from[])
   {
     ++i;
   }
+}
+
+// Exercise 1-17.
+void print_over_80_chars(void)
+{
+
+  int len;
+  char line[MAXLINE];
+
+  while ((len = my_getline(line, MAXLINE)) > 0)
+  {
+    if (len > 80)
+    {
+      printf("%s", line);
+    }
+  }
+}
+
+// Exercise 1-18.
+void delete_blanks(void)
+{
+  int len;
+  char line[MAXLINE];
+
+  while ((len = my_getline(line, MAXLINE)) > 0)
+  {
+    int i;
+    for (i = len - 2; i >= 0 && (line[i] == ' ' || line[i] == '\t'); --i)
+    {
+      line[i] = '\0';
+    }
+    if (i == -1) // all characters except the newline were removed, so delete this line
+    {
+      continue;
+    }
+    /*
+     * re-set the newline after deleting characters
+     * if no characters were deleted, i + 1 == len - 1, and line[len - 1] is already the newline
+     */
+    line[i + 1] = '\n';
+    printf("%s", line);
+  }
+}
+
+int main(void)
+{
+  // get_max_line();
+  // print_over_80_chars();
+  delete_blanks();
 }
