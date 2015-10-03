712be995b941c3361d58c1111efc2e74d003232f
Chris Copeland <chrisnc@cs.stanford.edu>
2015-10-03 00:04:42 -0700

    add exercise 2-5

--- strs.c
+++ strs.c
@@ -51,6 +51,22 @@ void squeeze_str(char s[], char e[])
   s[j] = '\0';
 }
 
+// Exercise 2-5.
+int any(const char s[], const char t[])
+{
+  for (int i = 0; s[i] != '\0'; ++i)
+  {
+    for (int j = 0; t[j] != '\0'; ++j)
+    {
+      if (s[i] == t[j])
+      {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
 int main(void)
 {
   char s[] = "hello, world";
@@ -61,7 +77,10 @@ int main(void)
   squeeze(s, c);
   printf("%s\"\n", s);
 
-  printf("squeeze_str(\"%s\", \'%s\') = \"", s, e);
+  printf("squeeze_str(\"%s\", \"%s\") = \"", s, e);
   squeeze_str(s, e);
   printf("%s\"\n", s);
+
+  char t[] = "re";
+  printf("any(\"%s\", \"%s\") = %d\n", s, t, any(s, t));
 }
