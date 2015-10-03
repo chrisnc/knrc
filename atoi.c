e00c14de9af7b8eb0cdd0c9a12dface2d4ab03e8
Chris Copeland <chrisnc@cs.stanford.edu>
2015-10-02 18:57:33 -0700

    add exercise 2-3, random number example, and my_lower function

--- atoi.c
+++ atoi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 // atoi: convert s to integer
 int my_atoi(const char s[])
@@ -11,8 +12,44 @@ int my_atoi(const char s[])
   return n;
 }
 
+// lower: convert c to lower case; ASCII only
+int my_lower(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+  {
+    c += 'a' - 'A';
+  }
+  return c;
+}
+
+// Exercise 2-3.
+int my_htoi(const char s[])
+{
+  int n = 0;
+  int i;
+  if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+  {
+    i = 2;
+  }
+  else
+  {
+    i = 0;
+  }
+  for (; isxdigit(s[i]); ++i)
+  {
+    int c = tolower(s[i]);
+    int next_digit = isalpha(c) ? c - 'a' + 10 : c - '0';
+    n = 0x10 * n + next_digit;
+  }
+  return n;
+}
+
 int main(void)
 {
   const char *s = "100";
+  const char *sx = "0x1a";
+  char c = 'F';
   printf("my_atoi(\"%s\") = %d\n", s, my_atoi(s));
+  printf("my_lower(\'%c\') = \'%c\'\n", c, my_lower(c));
+  printf("my_htoi(\"%s\") = %d\n", sx, my_htoi(sx));
 }
--- misc.c
+++ misc.c
@@ -19,6 +19,21 @@ void simple_loop(char s[], int lim)
   }
 }
 
+unsigned long int next = 1;
+
+// rand: return pseudo-random integer on 0..32767
+int my_rand(void)
+{
+  next = next * 1103515245 + 12345;
+  return (unsigned int)(next / 65536) & 32768;
+}
+
+// srand: set seed for rand()
+void my_srand(unsigned int seed)
+{
+  next = seed;
+}
+
 int main(void)
 {
   return 0;
