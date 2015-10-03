6178b834aafdd86f77189d33a646033fa7921bd8
Chris Copeland <chrisnc@cs.stanford.edu>
2015-10-03 12:22:53 -0700

    add bitcount example and popcnt for exercise 2-9

--- bits.c
+++ bits.c
@@ -36,6 +36,42 @@ unsigned rightrot(unsigned x, int n)
   return (x >> n) | (x << (s - n));
 }
 
+// bitcount: count 1 bits in x
+int bitcount(unsigned x)
+{
+  int b;
+  for (b = 0; x != 0; x >>= 1)
+  {
+    if (x & 0x1)
+    {
+      ++b;
+    }
+  }
+  return b;
+}
+
+// Exercise 2-9.
+// popcnt, faster version of bitcount
+// With x unsigned, x &= x - 1 deletes the rightmost 1-bit in x.
+// This is because the rightmost 1-bit will either be in the zero'th position
+// in which case subtracting 1 simply sets it to zero and leaves other bits
+// unchanged (and &'ing with x leaves this bit unset and the other bits
+// unchanged), or it is in some higher position with all lower-order bits zero,
+// in which case subtracting 1 sets this bit to zero and all previous bits to
+// 1. &'ing this with the original x sets all of these bits to zero, since the
+// rightmost 1 was set to zero in (x - 1), and the lower order bits were all
+// zero in x itself.
+int popcnt(unsigned x)
+{
+  int b = 0;
+  while (x)
+  {
+    ++b;
+    x &= x - 1;
+  }
+  return b;
+}
+
 int main(void)
 {
   unsigned x = 0xff00ff00;
@@ -49,4 +85,7 @@ int main(void)
   printf("invert(0x%x, %d, %d) = 0x%x\n", z, p, n, invert(z, p, n));
   n = -36;
   printf("rightrot(0x%x, %d) = 0x%x\n", z, n, rightrot(z, n));
+
+  printf("bitcount(0x%x) = %d\n", z, bitcount(z));
+  printf("popcnt(0x%x) = %d\n", z, popcnt(z));
 }
