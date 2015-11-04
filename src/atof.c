5ab018234a0992ec1ed97f0c863a4684df41e96b
Chris Copeland <chrisnc@cs.stanford.edu>
2015-11-04 10:17:01 -0800

    add exercise 4-2

--- src/atof.c
+++ src/atof.c
@@ -30,13 +30,44 @@ static double atof(const char *s)
     val = 10.0 * val + (s[i] - '0');
     power *= 10.0;
   }
+
+  // Exercise 4-2. page 73
+  // extend atof to allow an optional exponent
+  if (s[i] == 'e' || s[i] == 'E')
+  {
+    ++i;
+    int expsign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+    {
+      ++i;
+    }
+
+    int exponent = 0;
+    for (; isdigit(s[i]); ++i)
+    {
+      exponent = 10 * exponent + (s[i] - '0');
+    }
+
+    for (int e = 0; e < exponent; ++e)
+    {
+      if (expsign > 0)
+      {
+        power /= 10.0;
+      }
+      else
+      {
+        power *= 10.0;
+      }
+    }
+  }
+
   return sign * val / power;
 }
 
 int main(void)
 {
-  const char s[3][20] = {"-100.390", "   102", "+239.21340"};
-  for (size_t i = 0; i < 3; ++i)
+  const char s[5][20] = {"-100.390", "   102", "+239.21340", "3e3", "4.230e-2"};
+  for (size_t i = 0; i < 5; ++i)
   {
     printf("atof(\"%s\") = %f\n", s[i], atof(s[i]));
   }
