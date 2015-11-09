0d93a43254d67b7fee72c36196e1ea5bbdac3679
Chris Copeland <chrisnc@cs.stanford.edu>
2015-11-08 22:32:50 -0800

    add exercise 4-13

--- src/recur.c
+++ src/recur.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 // Exercise 4-12. page 88
 // NOTE: The return value is not the original value
@@ -34,15 +35,43 @@ static char *itoa_recur(int n, char *s)
   return s;
 }
 
+// Exercise 4-13. page 88
+static void reverse_recur(char *s)
+{
+  size_t n = strlen(s);
+  if (n < 2)
+  {
+    return;
+  }
+  char tmp = s[n - 1];
+  s[n - 1] = '\0';
+  reverse_recur(s + 1);
+  s[n - 1] = s[0];
+  s[0] = tmp;
+}
+
 int main(void)
 {
   char s[100] = {0};
-  int x[10] = {123, -1000, INT_MIN, INT_MAX, 0, -10000, 3, -10, -1, 1};
+  const int x[10] = {123, -1000, INT_MIN, INT_MAX, 0, -10000, 3, -10, -1, 1};
 
   for (size_t i = 0; i < 10; ++i)
   {
     itoa_recur(x[i], s);
     printf("itoa_recur(%d, s) -> s = \"%s\"\n", x[i], s);
+  }
+
+  char a[] = "hello, there";
+  char b[] = "this is a string";
+  char c[] = "";
+  char d[] = "0012";
+  char *const ss[] = {a, b, c, d};
+
+  for (size_t i = 0; i < 4; ++i)
+  {
+    printf("reverse_recur(\"%s\") ->", ss[i]);
+    reverse_recur(ss[i]);
+    printf(" \"%s\"\n", ss[i]);
   }
 
   return 0;
