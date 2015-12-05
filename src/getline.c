d4bfa9eb9fbf4d34fee3d99f2a98d6ba500500e8
Chris Copeland <chrisnc@cs.stanford.edu>
2015-12-05 15:03:08 -0800

    simplify error handling in getline.c

--- src/getline.c
+++ src/getline.c
@@ -4,6 +4,11 @@
 
 size_t my_getline(char *s, size_t lim)
 {
+  if (s == NULL || lim == 0)
+  {
+    return 0;
+  }
+
   // Exercise 5-6. page 107
   // modified to use pointer arithmetic
   const char *begin = s;
@@ -13,9 +18,6 @@ size_t my_getline(char *s, size_t lim)
   {
     *s++ = (char)c;
   }
-  if (lim > 0)
-  {
-    *s = '\0';
-  }
+  *s = '\0';
   return (size_t)(s - begin);
 }
