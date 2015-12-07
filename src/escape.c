fc30d33a336b9a74417141c56adcf0de606c4658
Chris Copeland <chrisnc@cs.stanford.edu>
2015-12-07 00:09:01 -0800

    build with asan and ubsan on Linux
    unescape had an invalid read if the last character
    was a slash
    
    converted both escape.c and unescape.c to go character
    by character (going by lines didn't make sense especially
    for unescape, since usually newlines would be escaped in its input)

--- src/escape.c
+++ src/escape.c
@@ -1,45 +1,29 @@
 #include <stdio.h>
 
-#include "getline.h"
-
 // Exercise 3-2. page 60
-static void escape(char *s, const char *t)
-{
-  while (*t)
-  {
-    switch (*t)
-    {
-    case '\t':
-      *s++ = '\\';
-      *s++ = 't';
-      break;
-    case '\n':
-      *s++ = '\\';
-      *s++ = 'n';
-      break;
-    case '\\':
-      *s++ = '\\';
-      *s++ = '\\';
-      break;
-    default:
-      *s++ = *t;
-      break;
-    }
-    ++t;
-  }
-  *s = '\0';
-}
-
-#define MAXLINE 1000
 
 int main(void)
 {
-  char line[MAXLINE];
-  char escaped[MAXLINE * 2];
-  size_t len;
-  while ((len = my_getline(line, MAXLINE)) > 0)
+  int c;
+  while ((c = getchar()) != EOF)
   {
-    escape(escaped, line);
-    printf("%s", escaped);
+    switch (c)
+    {
+    case '\t':
+      putchar('\\');
+      putchar('t');
+      break;
+    case '\n':
+      putchar('\\');
+      putchar('n');
+      break;
+    case '\\':
+      putchar('\\');
+      putchar('\\');
+      break;
+    default:
+      putchar(c);
+      break;
+    }
   }
 }
--- src/unescape.c
+++ src/unescape.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
-
-#include "getline.h"
+#include <stdbool.h>
 
 // Exercise 3-2. part 2 page 60
-static void unescape(char *s, const char *t)
-{
-  while (*t)
-  {
-    if (*t == '\\')
-    {
-      ++t;
-      switch (*t)
-      {
-      case 'n':
-        *s++ = '\n';
-        break;
-      case 't':
-        *s++ = '\t';
-        break;
-      default:
-        *s++ = *t;
-        break;
-      }
-      ++t;
-    }
-    else
-    {
-      *s++ = *t++;
-    }
-  }
-  *s = '\0';
-}
-
-#define MAXLINE 1000
 
 int main(void)
 {
-  char line[MAXLINE];
-  char unescaped[MAXLINE];
-  size_t len;
-  while ((len = my_getline(line, MAXLINE)) > 0)
+  int c;
+  bool escaped = false;
+  while ((c = getchar()) != EOF)
   {
-    unescape(unescaped, line);
-    printf("%s", unescaped);
+    if (escaped)
+    {
+      switch (c)
+      {
+      case 'n':
+        putchar('\n');
+        break;
+      case 't':
+        putchar('\t');
+        break;
+      default:
+        putchar(c);
+        break;
+      }
+      escaped = false;
+    }
+    else
+    {
+      if (c == '\\')
+      {
+        escaped = true;
+      }
+      else
+      {
+        putchar(c);
+      }
+    }
   }
 }
