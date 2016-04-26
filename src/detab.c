d7f83e9beadd32737506b718308c41da80665693
Chris Copeland <chrisnc@cs.stanford.edu>
2016-04-25 23:26:10 -0700

    add exercise 5-11 and 5-12 for entab.c and detab.c

--- src/detab.c
+++ src/detab.c
@@ -1,24 +1,58 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "getline.h"
 
 // Exercise 1-20. page 34
 
-#define TABSTOP 8
+// Exercise 5-11. page 118
+// modified to accept a list of tabstops in argv
+
+#define TABSTOP_DEFAULT 8
 #define MAXLINE 1000
 
-int main(void)
+int main(int argc, char **argv)
 {
+  size_t m = 0;
+  size_t n = TABSTOP_DEFAULT;
+  while (--argc > 0)
+  {
+    ++argv;
+    if (**argv == '-' && sscanf(*argv + 1, "%zu", &m) != 1)
+    {
+      fprintf(stderr, "detab: illegal option -%s\n", *argv);
+      return EXIT_FAILURE;
+    }
+    if (**argv == '+' && sscanf(*argv + 1, "%zu", &n) != 1)
+    {
+      fprintf(stderr, "detab: illegal option %s\n", *argv);
+      return EXIT_FAILURE;
+    }
+  }
+
   char line[MAXLINE];
   size_t len;
+
   while ((len = my_getline(line, MAXLINE)) > 0)
   {
-    int col = 0;
+    bool first_tabstop = true;
+    size_t col = 0;
     for (size_t i = 0; i < len; ++i)
     {
       if (line[i] == '\t')
       {
-        for (size_t rem = TABSTOP - (col % TABSTOP); rem > 0; --rem)
+        size_t tabstop;
+        if (*argv || sscanf(*argv++, "%zu", &tabstop) != 1)
+        {
+          tabstop = n;
+        }
+        if (first_tabstop)
+        {
+          tabstop += m;
+          first_tabstop = false;
+        }
+        for (size_t rem = tabstop - ((col - m) % tabstop); rem > 0; --rem)
         {
           ++col;
           putchar(' ');
--- src/entab.c
+++ src/entab.c
@@ -1,26 +1,63 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "getline.h"
 
 // Exercise 1-21. page 34
 
-#define TABSTOP 8
+// Exercise 5-11. page 118
+// modified to accept a list of tabstops in argv
+
+// Exercise 5-12. page 118
+// modified to accept -m +n for tab stops every N columns
+// starting at column m.
+
+#define TABSTOP_DEFAULT 8
 #define MAXLINE 1000
 
-int main(void)
+int main(int argc, char **argv)
 {
+  size_t m = 0;
+  size_t n = TABSTOP_DEFAULT;
+  while (--argc > 0)
+  {
+    ++argv;
+    if (**argv == '-' && sscanf(*argv + 1, "%zu", &m) != 1)
+    {
+      fprintf(stderr, "entab: illegal option -%s\n", *argv);
+      return EXIT_FAILURE;
+    }
+    if (**argv == '+' && sscanf(*argv + 1, "%zu", &n) != 1)
+    {
+      fprintf(stderr, "entab: illegal option %s\n", *argv);
+      return EXIT_FAILURE;
+    }
+  }
+
   char line[MAXLINE];
   size_t len;
 
   while ((len = my_getline(line, MAXLINE)) > 0)
   {
     size_t next_t;
+    size_t tabstop;
+    bool first_tabstop = true;
     for (size_t t = 0; t < len; t = next_t)
     {
-      next_t = t + TABSTOP;
+      if (*argv || sscanf(*argv++, "%zu", &tabstop) != 1)
+      {
+        tabstop = n;
+      }
+      if (first_tabstop)
+      {
+        tabstop += m;
+        first_tabstop = false;
+      }
+      next_t = t + tabstop;
       size_t blanks_to_replace = 0;
       size_t i;
-      for (i = t; i < t + TABSTOP && i < len; ++i)
+      for (i = t; i < t + tabstop && i < len; ++i)
       {
         if (line[i] == ' ')
         {
@@ -29,7 +66,7 @@ int main(void)
         else if (line[i] == '\t')
         {
           next_t = i + 1;
-          blanks_to_replace = TABSTOP;
+          blanks_to_replace = tabstop;
           break;
         }
         else
