a6825b3b3cff0932e8bb63b50fdaba568f79ba95
Chris Copeland <chrisnc@cs.stanford.edu>
2016-07-17 21:00:35 -0400

    add undcl example

--- src/dcl.c
+++ src/dcl.c
@@ -81,7 +81,8 @@ static void dirdcl(void)
   }
 }
 
-int main(void)
+// dcl: convert declaration to words, page 125
+static int main_dcl(void)
 {
   while (gettoken() != EOF)
   {
@@ -94,6 +95,51 @@ int main(void)
     }
     printf("%s: %s %s\n", name, out, datatype);
   }
+  return 0;
+}
+
+// undcl: convert word description to declaration, page 126
+static int main_undcl(void)
+{
+  int type;
+  char temp[MAXTOKEN];
+
+  while (gettoken() != EOF)
+  {
+    strcpy(out, token);
+    while ((type = gettoken()) != '\n')
+    {
+      if (type == PARENS || type == BRACKETS)
+      {
+        strcat(out, token);
+      }
+      else if (type == '*')
+      {
+        sprintf(temp, "(*%s)", out);
+        strcpy(out, temp);
+      }
+      else if (type == NAME)
+      {
+        sprintf(temp, "%s %s", token, out);
+        strcpy(out, temp);
+      }
+      else
+      {
+        printf("invalid input at %s\n", token);
+      }
+    }
+    printf("%s\n", out);
+  }
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 1 && strcmp(argv[1], "-u") == 0)
+  {
+    return main_undcl();
+  }
+  return main_dcl();
 }
 
 static int gettoken(void)
